8e1aa3b9c3e277afb6ccd0761510582c242aa45a
Rachit Dev <rachit.dev@gmail.com>
2017-06-21 13:56:19 +0530

    K2uxBooksTest.java 's code is more refined and is more fast than before. Also comments added for serialization logic.

--- src/main/java/com/learningservices/testscripts/K2uxBooksTest.java
+++ src/main/java/com/learningservices/testscripts/K2uxBooksTest.java
@@ -46,42 +46,35 @@ public class K2uxBooksTest extends BrowserDriver {
 	
 	@SuppressWarnings("static-access")
 	@Test(enabled = true, description = "Testing multiple books")
-	public void verifybooknavigationbuttons() throws InterruptedException, Exception  {
-//		HashMap<String, String> userData = DataUtils.testDatabyID("tc_01", "LoginCredentials");
-//		K2uxLoginPage login = new K2uxLoginPage(driver);
-//		K2uxBookPage book = new K2uxBookPage(driver);
-//		String username = userData.get("Username");
-//		String password = userData.get("Password");
-//		login.enterUsername(username);
-//		login.enterPassword(password);
-//		book.click(driver, K2uxLoginPage.loginButton); //login complete.
+	public void testMultipleBooks() throws InterruptedException, Exception  {
 		K2uxLoginPage login = new K2uxLoginPage(driver);
 		K2uxBookPage book = new K2uxBookPage(driver);
 		login.autoLogin("tc_01");
 		//Iterator logic to iterate through first 10 books.
-		
 		for (int i=1;i<=10;i++)
 		{      
 		Serializable itr = String.valueOf(i);
 		Serializable bid = "BID_" + itr;
 		Log.message("The xpath to be accessed has an id: "+bid);
-		//starting logic to access book from excel sheet.
+		//Starting logic to access xpath from excel sheet.
 		HashMap<String, String> bookData = DataUtils.testBookbyID(bid, "BookSelection");
 		String book_xpath = bookData.get("Book_Xpath");
-		if (driver.findElement(By.xpath(book_xpath)) != null)
+		if (book_xpath != null && driver.findElement(By.xpath(book_xpath)).isEnabled())
 			{
 		bookElement = driver.findElement(By.xpath(book_xpath));
 		Log.message("Found "+ bookElement +" to click and open the book!");
+		//Logic to find xpath from excel sheet ends.
 		book.click(driver, bookElement);
 		book.verify(driver, K2uxBookPage.disabledpreviouspagebutton);
 		book.click(driver, K2uxBookPage.backtobookshelf);
 		Log.testCaseResult();
 		Log.endTestCase();
-			}if (driver.findElement(By.xpath(book_xpath)) == null){
+			}if (book_xpath == null){
 		Log.message(bookElement +" not found," + " Or Serialization Failed!");
+		//Serialization failed.
 		Log.testCaseResult();
 		Log.endTestCase();
-					}
+									}
 		}
 		book.click(driver, K2uxLoginPage.k2avatarButton);
 		book.click(driver, K2uxLoginPage.signout);
